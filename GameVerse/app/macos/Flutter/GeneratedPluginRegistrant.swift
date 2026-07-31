16bba03795053ac188283e713375324d3cc24987
Giovanny <contrerasgiovanny71@gmail.com>
2026-07-30 22:43:19 -0500

    Perfil y registro actualizado

--- GameVerse/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ GameVerse/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,7 +14,6 @@ import package_info_plus
 import shared_preferences_foundation
 import url_launcher_macos
 import video_player_avfoundation
-import volume_controller
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -27,6 +26,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
-  VolumeControllerPlugin.register(with: registry.registrar(forPlugin: "VolumeControllerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
